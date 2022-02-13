c2abec1db154c1dad14aab2a12dfff4c260b09dd
ifenega <69639702+ifenega@users.noreply.github.com>
2022-02-13 16:01:44 +0100

    final commit

--- script.js
+++ script.js
@@ -8,15 +8,55 @@ console.log(img)
 
 
 
-li.forEach(addEventListener('click', (e)=>{
-    if(e.target.classList.contains('img4')|| e.target.classList.contains('faq')||  e.target.classList.contains('p-tag')) {
-        var j = e.target.children[1];
-       j.classList.toggle('active')
-    
-    }
-    console.log(e.target)
-    
-}))
+li.forEach(function (i) {
+    i.addEventListener('click', (e)=>{
+        if( e.target.classList.contains('faq')) {
+            let j = e.target.children[1];
+            let b= e.target.children[2]
+           j.classList.toggle('active')
+           b.classList.toggle('active')
+           b.addEventListener('click',(e) => {
+               if (e.target.classList.contains('active')) {
+                   j.classList.toggle('active')
+                   b.classList.toggle('active')
+               }
+           })
+        } else if(e.target.classList.contains('img4')) {
+            var k = e.target.nextElementSibling;
+            k.classList.toggle('active')
+            e.target.classList.toggle('active')
+                
+        } 
+        else {
+            var m = e.target.nextElementSibling;
+            var z = e.target.nextElementSibling.nextElementSibling
+            console.log(z)
+            m.classList.toggle('active')
+            z.classList.toggle('active')
+        }
+        
+        
+        
+        
+        
+       
+
+        
+    })
+
+   
+})
+
+// li.forEach(addEventListener('click', e=> {
+//     if(e.target.classList.contains('img4')) {
+//         var m = e.target.parentElement;
+//         m.child[2].classList.toggle("active")
+//     }
+// }))
+
+
+
+
 
 
 
